a45204a5e47ab4bcb661f7d5678d45b36520edc1
Antoine Merino <antoine.merino.dev@gmail.com>
2025-01-10 01:14:06 +0100

    WIP: memcached optimization with sliding window-counter

--- limits/resources/redis/lua_scripts/acquire_sliding_window.lua
+++ limits/resources/redis/lua_scripts/acquire_sliding_window.lua
@@ -11,7 +11,7 @@ end
 local current_ttl = tonumber(redis.call('pttl', KEYS[2]))
 
 if current_ttl > 0 and current_ttl < expiry then
-
+    -- Current window expired, shift it to the previous window
     redis.call('rename', KEYS[2], KEYS[1])
     redis.call('set', KEYS[2], 0, 'PX', current_ttl + expiry)
 end
@@ -21,24 +21,24 @@ local previous_ttl = tonumber(redis.call('pttl', KEYS[1])) or 0
 local current_count = tonumber(redis.call('get', KEYS[2])) or 0
 current_ttl = tonumber(redis.call('pttl', KEYS[2])) or 0
 
+-- If the values don't exist yet, consider the TTL is 0
 if previous_ttl <= 0 then
     previous_ttl = 0
 end
 if current_ttl <= 0 then
     current_ttl = 0
 end
-
 local weighted_count = previous_count * previous_ttl / expiry + current_count
 
 if (weighted_count + amount) > limit then
     return false
 end
 
--- Check if the key exists
+-- If the current counter exists, increase its value
 if redis.call('exists', KEYS[2]) == 1 then
     redis.call('incrby', KEYS[2], amount)
 else
-    -- If the current doesn't exist, set the value with twice the expiry time
+    -- Otherwise, set the value with twice the expiry time
     redis.call('set', KEYS[2], amount, 'PX', expiry * 2)
 end
 
--- test_app/app.py
+++ test_app/app.py
@@ -1,12 +1,14 @@
-import redis
 from fastapi import FastAPI, Request
-from fastapi.responses import PlainTextResponse, Response
+from fastapi.responses import PlainTextResponse
 from slowapi import Limiter, _rate_limit_exceeded_handler
 from slowapi.errors import RateLimitExceeded
 from slowapi.util import get_remote_address
 
-redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
-limiter = Limiter(key_func=get_remote_address, strategy="sliding-window-counter", storage_uri="redis://localhost:6379/0")
+# redis_client = redis.StrictRedis(host='localhost', port=6379, db=0, decode_responses=True)
+
+# limiter = Limiter(key_func=get_remote_address, strategy="sliding-window-counter", storage_uri="redis://localhost:7379/0")
+limiter = Limiter(key_func=get_remote_address, strategy="sliding-window-counter", storage_uri="memcached://localhost:22122")
+
 # limiter = Limiter(key_func=get_remote_address, strategy="sliding-window-counter")
 
 app = FastAPI()
@@ -16,6 +18,6 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 # Note: the route decorator must be above the limit decorator, not below it
 @app.get("/home")
-@limiter.limit("1/1 second")
+@limiter.limit("2/1 second")
 async def homepage(request: Request):
     return PlainTextResponse("test")
